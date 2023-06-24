7642e0d048107fc86ffbca99989f4d45d72bfe87
YAA <yudovanton@yandex.ru>
2023-06-25 00:06:05 +0300

    # Задачки A и B

--- ProjectOne/HelloWorld.cpp
+++ ProjectOne/HelloWorld.cpp
@@ -1,12 +1,11 @@
-#include <iostream>
+п»ї#include <iostream>
 #include <string>
 
 int main() {
-	int a;
-	int b;
-	int c;
-	std::cin >> a >> b >> c;  // Поочередно принемает значения разделеные переход на новую строку либо пробелом
-	std::cout << a << " " << b << " " << c << "\n";
+	int a, b;
+	std::cin >> a >> b;
+	std::cout << a + b;
+	return 0;
 }
 
 
@@ -35,13 +34,13 @@ int name() {
 
 int FullName() {
 	/*
-		Чтобы считать в строковую переменную всю строчку целиком
-		(не включая завершающий символ перевода строки),
-		нужно использовать функцию <std::getline> из заголовочного файла <string>.
+		Р§С‚РѕР±С‹ СЃС‡РёС‚Р°С‚СЊ РІ СЃС‚СЂРѕРєРѕРІСѓСЋ РїРµСЂРµРјРµРЅРЅСѓСЋ РІСЃСЋ СЃС‚СЂРѕС‡РєСѓ С†РµР»РёРєРѕРј
+		(РЅРµ РІРєР»СЋС‡Р°СЏ Р·Р°РІРµСЂС€Р°СЋС‰РёР№ СЃРёРјРІРѕР» РїРµСЂРµРІРѕРґР° СЃС‚СЂРѕРєРё),
+		РЅСѓР¶РЅРѕ РёСЃРїРѕР»СЊР·РѕРІР°С‚СЊ С„СѓРЅРєС†РёСЋ <std::getline> РёР· Р·Р°РіРѕР»РѕРІРѕС‡РЅРѕРіРѕ С„Р°Р№Р»Р° <string>.
 	*/
 	std::string name;
 	std::cout << "What is your name?\n";
-	std::getline(std::cin, name);  // <std::getline(?istream&?is,?string&?str,?char?delim)>
+	std::getline(std::cin, name);  // <std::getline(вЂЇistream&вЂЇis,вЂЇstring&вЂЇstr,вЂЇcharвЂЇdelim)>
 	std::cout << "Hello, " << name << "!\n";
 	return 0;
 }
@@ -51,6 +50,24 @@ int ThreeNumbers() {
 	int a;
 	int b;
 	int c;
-	std::cin >> a >> b >> c;  // Поочередно принемает значения разделеные переход на новую строку либо пробелом
+	std::cin >> a >> b >> c;  // РџРѕРѕС‡РµСЂРµРґРЅРѕ РїСЂРёРЅРµРјР°РµС‚ Р·РЅР°С‡РµРЅРёСЏ СЂР°Р·РґРµР»РµРЅС‹Рµ РїРµСЂРµС…РѕРґ РЅР° РЅРѕРІСѓСЋ СЃС‚СЂРѕРєСѓ Р»РёР±Рѕ РїСЂРѕР±РµР»РѕРј
 	std::cout << a << " " << b << " " << c << "\n";
+	return 0;
+}
+
+
+int TaskTwoA() {
+	std::cout << "C++ is a general-purpose programming language with a bias towards systems programming that\n";
+	std::cout << "  - is a better C\n";
+	std::cout << "  - supports data abstraction\n";
+	std::cout << "  - supports object-oriented programming\n";
+	std::cout << "  - supports generic programming.";
+	return 0;
+}
+
+int TaskTwoB() {
+	int a, b;
+	std::cin >> a >> b;
+	std::cout << a + b;
+	return 0;
 }
