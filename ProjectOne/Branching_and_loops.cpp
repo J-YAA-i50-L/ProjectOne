8eaf46b28f2c3fc9cbf5b112f2478d1823a1cac9
Anton <97762136+J-YAA-i50-L@users.noreply.github.com>
2023-09-09 19:40:45 +0000

    +-

--- ProjectOne/Branching_and_loops.cpp
+++ ProjectOne/Branching_and_loops.cpp
@@ -6,8 +6,15 @@
 
 #include <iostream>
 
-int main(){
-    
+int main() {
+    double x = 0.1, y = 0.2;
+    if (x + y == 0.3) {
+        std::cout << "EQUAL ";
+    } else {
+        std::cout << "NOT EQUAL ";
+    }
+ 
+    std::cout << x + y << "\n";
 }
 
 
@@ -76,3 +83,4 @@ int DifficultConditions() {
     }
     return 0;
 }
+
